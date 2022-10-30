60ff3de2e786f30a69cfcd77ddc6a241c797d2ad
Илья Щербаков <banderolfromacer@yandex.ru>
2022-10-30 22:09:22 +0300

    Подправил settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -117,8 +117,8 @@ DATABASES = {
         'ENGINE': os.getenv('DB_ENGINE',
                             default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('DB_USER', default='foodgram_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', default='xxxyyyzzz'),
+        'USER': os.getenv('POSTGRES_USER', default='foodgram_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'),
         'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
