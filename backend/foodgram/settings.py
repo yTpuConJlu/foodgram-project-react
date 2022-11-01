8e0b61d4fd6916ed5be4e0ad7622906a26a80a07
Илья Щербаков <banderolfromacer@yandex.ru>
2022-11-01 03:08:17 +0300

    Настройка деплоя

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -116,8 +116,8 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE',
                             default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER', default='foodgram'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='xxxyyyzzz'),
         'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
         'PORT': os.getenv('DB_PORT', default='5432')
